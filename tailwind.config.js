da981df760d57248190d563282c2c290d61ea8d1
SADEK HOSSAIN <shadeks16@gmail.com>
2022-05-21 23:57:25 +0600

    project-setup-done

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FFD800",
+          secondary: "#F6621B",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
