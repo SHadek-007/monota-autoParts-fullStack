1b0067cdf9628057e68bebbd4b86c38bfee0a588
SADEK HOSSAIN <shadeks16@gmail.com>
2022-05-22 00:30:24 +0600

    header-part-done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
     <div>
-      <h2>Hi Daisy</h2>
-      <button class="btn btn-primary">Button</button>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
