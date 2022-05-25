eed42c093854794250fea4fd421e86fe0f7ca06a
SADEK HOSSAIN <shadeks16@gmail.com>
2022-05-25 13:58:36 +0600

    review part done

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Banner from './Banner';
 import Categories from './Categories';
 import OurBenifits from './OurBenifits';
 import Parts from './Parts/Parts';
-import Review from './Review';
 import Reviews from './Reviews';
 import Summery from './Summery';
 
 
 const Home = () => {
+    const [reviewList, setReviewList] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:5000/review")
+          .then((res) => res.json())
+          .then((data) => setReviewList(data));
+      }, []);
+
     return (
         <div>
             <Banner></Banner>
@@ -20,8 +26,14 @@ const Home = () => {
             <Summery></Summery>
             <div className='px-5 lg:px-12'>
             <OurBenifits></OurBenifits>
-            <Review></Review>
-            <Reviews></Reviews>
+            </div>
+            <div className='px-5 lg:px-12'>
+                <h2 className='text-2xl text-center font-semibold'>Reviews</h2>
+            <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5'>
+                {
+                    reviewList.map(review => <Reviews review={review}></Reviews>)
+                }
+            </div>
             </div>
             
         </div>
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Reviews = ({rev}) => {
-    console.log(rev);
-    return (
-        <div>
-            <h2></h2>
-        </div>
-    );
+const Reviews = ({ review }) => {
+  return (
+    <div class="card max-w-lg mb-12 mt-8 bg-success text-success-content shadow-xl">
+      <div class="card-body">
+        <h2 class="card-title">{review.user}</h2>
+        <p>Rating: <span className="text-secondary font-semibold">{review.rating}</span></p>
+        <p>“ <small>{review.description.slice(0,100)}</small> ”</p>
+      </div>
+    </div>
+  );
 };
 
 export default Reviews;
